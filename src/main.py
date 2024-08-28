24e1c149c7073fac0ba773645f31c6feb9b1f017
GniLudio <ferdinand.rohlfing@googlemail.com>
2024-08-28 15:19:47 +0200

    Add main.py

--- src/main.py
+++ src/main.py
@@ -10,13 +10,13 @@ models = ["RandomForestClassifier", "GaussianNB", "KNeighborsClassifier", "Linea
 with utils.TimeLogger("Extracting Features", "Extracting Features\tDone\t{duration:.2f}"):
     for window_size in window_sizes:
         for window_overlap in window_overlaps:
-            with utils.TimeLogger(f"\t{window_size} - {window_overlap}", f"\t{window_size} - {window_overlap}""Done\t{duration:.2f}", separator="\t"):
-                subprocess.run(["python", os.path.join("src", "extract_features.py"), "-ws", str(window_size), "-wo", str(window_overlap)])
+            with utils.TimeLogger(f"\t{window_size} - {window_overlap}", "Done\t{duration:.2f}", separator="\t"):
+                subprocess.run(["python", os.path.join("src", "extract_features.py"), "-ws", str(window_size), "-wo", str(window_overlap)], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
 
 # Cross Validation
 with utils.TimeLogger("Cross Validating", "Cross Validating\tDone\t{duration:.2f}"):
     for window_size in window_sizes:
         for window_overlap in window_overlaps:
             for model in models:
-                with utils.TimeLogger(f"\t{window_size} - {window_overlap} - {model}", f"\t{window_size} - {window_overlap} - {model}\tDone\t""{duration:.2f}"):
-                    subprocess.run(["python", os.path.join("src", "cross_validation.py"), "-dp", "-ws", str(window_size), "-wo", str(window_overlap)])
+                with utils.TimeLogger(f"\t{window_size} - {window_overlap} - {model}", "Done\t""{duration:.2f}", separator="\t"):
+                    subprocess.run(["python", os.path.join("src", "cross_validation.py"), "-dp", "-ws", str(window_size), "-wo", str(window_overlap)], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
