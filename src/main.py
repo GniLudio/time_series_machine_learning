8cbe86a2f9e4e2715cf49bd3a15a9632e9f3cf67
GniLudio <ferdinand.rohlfing@googlemail.com>
2024-08-28 18:01:20 +0200

    Fix main.py

--- src/main.py
+++ src/main.py
@@ -19,4 +19,4 @@ with utils.TimeLogger("Cross Validating", "Cross Validating\tDone\t{duration:.2f
         for window_overlap in window_overlaps:
             for model in models:
                 with utils.TimeLogger(f"\t{window_size} - {window_overlap} - {model}", "Done\t""{duration:.2f}", separator="\t"):
-                    subprocess.run(["python", os.path.join("src", "cross_validation.py"), "-dp", "-ws", str(window_size), "-wo", str(window_overlap)], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+                    subprocess.run(["python", os.path.join("src", "cross_validation.py"), "-pd", "-ws", str(window_size), "-wo", str(window_overlap)], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
