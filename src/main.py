ecfe74042c3363824ac0acd46f1d304c9e71fb8c
GniLudio <ferdinand.rohlfing@googlemail.com>
2024-09-02 13:33:47 +0200

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,20 +1,25 @@
 import subprocess
 import os
 import utils
+import itertools
+import tsml
 
-window_sizes = [100, 200, 250, 500, 1000, 2000, 4000]
-window_overlaps = [0, 1, 2, 3, 4, 5, 10, 25, 50] 
-models = ["RandomForestClassifier", "GaussianNB", "KNeighborsClassifier", "LinearSVC", "MLPClassifier", "RandomForestClassifier", "SVC"]
+window_sizes = [100]
+window_overlaps = [0] 
+models = ["RandomForestClassifier"]
 
 # Extracting Features
-with utils.TimeLogger("Extracting Features", "Extracting Features\tDone\t{duration:.2f}"):
-    for window_size in window_sizes:
-        for window_overlap in window_overlaps:
-            subprocess.run(["python", os.path.join("src", "extract_features.py"), "-ws", str(window_size), "-wo", str(window_overlap)])
+#with utils.TimeLogger("Extracting Features", "Extracting Features\tDone\t{duration:.2f}"):
+#    for window_size in window_sizes:
+#        for window_overlap in window_overlaps:
+#            subprocess.run(["python", os.path.join("src", "extract_features.py"), "-ws", str(window_size), "-wo", str(window_overlap)])
 
 # Cross Validation
 with utils.TimeLogger("Cross Validating", "Cross Validating\tDone\t{duration:.2f}"):
+    feature_combinations: list[tuple[str, str, str]] = itertools.combinations(tsml.TSFEL_FEATURES, 2)
+    feature_combinations: list[str] = [[f"-f '{feature}'" for feature in list(combination)] for combination in feature_combinations]
     for window_size in window_sizes:
         for window_overlap in window_overlaps:
             for model in models:
-                subprocess.run(["python", os.path.join("src", "cross_validation.py"), "-pd", "-ws", str(window_size), "-wo", str(window_overlap), "-m", model])
+                for feature_combination in feature_combinations:
+                    subprocess.run(["python", os.path.join("src", "cross_validation.py"), "-pd", "-ws", str(window_size), "-wo", str(window_overlap), "-m", model])
