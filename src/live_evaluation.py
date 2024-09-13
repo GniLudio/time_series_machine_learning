3c6fb7c201490553dd9e52418283bb02117809af
GniLudio <ferdinand.rohlfing@googlemail.com>
2024-09-13 02:12:27 +0200

    Implement live_evaluation.py

--- src/live_evaluation.py
+++ src/live_evaluation.py
@@ -1,51 +1,77 @@
-import pylsl
 import argparse
-import pandas
-import sklearn
-import sklearn.ensemble
-from sklearn.datasets import make_classification
-import tsml
-import models
+
+import tsfel.feature_extraction
 from utils import TimeLogger
-from extract_features import load_features
+import train_model
+import pickle
+import pylsl
+import keyboard
+import time
+import tsfel
+
+import tsml
 
 def fetch_time_series():
-    global time_series_inlet
+    global time_series_inlet, time_series_buffer
+
+    (samples, _) = time_series_inlet.pull_chunk()
+    time_series_buffer.extend(samples)
+
+def update_evaluation():
+    global time_series_buffer, tsfel_configuration, model, prediction
+    features = tsfel.time_series_features_extractor(
+        dict_features=tsfel_configuration, 
+        signal_windows=time_series_buffer,
+        fs=tsml.SAMPLING_FREQUENCY,
+        verbose=0,
+    )
+    prediction = model.predict(features)
+    time_series_buffer.clear()
+    print("Updated Prediction", prediction)
+
+def on_esc():
+    global exited
+    exited = True
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser("Run the live evalution with the following parameters:")
-    parser.add_argument("-i", "--interval", default=100, type=int, help="The evaluation interval.")
-    parser.add_argument("-d", "--domain", default="all", type=str, choices=tsml.TSFEL_FEATURE_DOMAINS, help="The tsfel feature domain to be used.")
-    parser.add_argument("-ws", "--window_size", default=100, type=int, help="The window size to be used. (in ms)")
-    parser.add_argument("-wo", "--window_overlap", default=20, type=int, help="The window overlap to be used.")
-    parser.add_argument("-m", "--model", default="RandomForestClassifier", choices=models.MODELS.keys(), help="The model to be used.")
+    parser.add_argument("-i", "--input-filename", required=True, type=str, help="The input filename of the trained model.")
+    parser.add_argument("-f", "--frequency", default=100, type=int, help="The evaluation interval. (in ms)")
+    parser.add_argument("-d", "--domain", default="all", type=str, choices=tsml.TSFEL_FEATURE_DOMAINS, help="The tsfel domain to be used. (all if omitted)")
+    parser.add_argument("-ws", "--window-size", default=100, type=int, help="The window size. (in ms)")
     args = parser.parse_args()
 
     start_end_logger = TimeLogger(f"live_evaluation.py\tStart\t{args}", "live_evaluation.py\tDone\t{duration:0.2f}")
     start_end_logger.start()
 
     # Parameters
-    interval = args.interval
+    input_filename = args.input_filename
+    frequency = args.frequency
     domain = args.domain
     window_size = args.window_size
-    window_overlap = args.window_overlap
-    model_name = args.model
 
-    with TimeLogger("Setup Model", "Done\t{duration:.2f}", separator="\t"):
-        model: sklearn.ensemble.RandomForestClassifier = models.MODELS[model_name]()
+    tsfel_configuration = tsfel.get_features_by_domain(domain != "all" and domain or None)
 
-    with TimeLogger("Loading Features", "Done\t{duration:.2f}", separator="\t"):
-        dataset = load_features(domain, window_size, window_overlap)
-        dataset.sample(n=len(dataset) // 10_000)
+    with TimeLogger("Load Model", "Done\t{duration:.2f}", separator="\t"):
+        with open(train_model.get_filename(input_filename), 'rb') as file:
+            model = pickle.load(file)
 
-    with TimeLogger("Training Model", "Done\t{duration:.2f}", separator="\t"):
-        X = dataset.drop(columns=tsml.ADDITIONAL_COLUMNS, inplace=False)
-        Y = dataset[tsml.LABEL_COLUMN]
-        model.fit(X, Y)
+    with TimeLogger("Setup OpenSignals Stream"	, "Done\t{duration:.2f}", separator="\t"):
+        time_series_stream_info = next((stream_info for stream_info in pylsl.resolve_streams() if stream_info.name() == "OpenSignals"), None)
+        time_series_inlet = time_series_stream_info is not None and pylsl.StreamInlet(time_series_stream_info) or None
+        time_series_buffer: list[list[float]] = []
 
-    #with TimeLogger("Setup OpenSignals Stream"	, "Done\t{duration:.2f}", separator="\t"):
-    #    time_series_stream_info = next((stream_info for stream_info in pylsl.resolve_streams() if stream_info.name() == "OpenSignals"), None)
-    #    time_series_inlet = time_series_stream_info is not None and pylsl.StreamInlet(time_series_stream_info) or None
-    #    time_series_buffer: list[list[float]] = []
+    # Main Loop
+    prediction = 0
+    last_evaluation_time = 0
+    exited = False
+    keyboard.hook(lambda _: on_esc())
+    while not exited:
+        fetch_time_series()
+        if time.time() - last_evaluation_time > frequency / 1000:
+            last_evaluation_time = time.time()
+            update_evaluation()
+        time.sleep(0)
+
 
     start_end_logger.end()
---
+++ src/train_model.py
@@ -0,0 +1,49 @@
+import argparse
+import tsml
+import models
+import pickle
+import os
+import time
+import tsfel
+import pylsl
+
+import extract_features
+from utils import TimeLogger
+
+def get_filename(filename: str):
+    return os.path.join(tsml.TRAINED_MODEL_DIRECTORY, filename + ".pickle")
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser("Run the live evalution with the following parameters:")
+    parser.add_argument("-d", "--domain", default="all", type=str, choices=tsml.TSFEL_FEATURE_DOMAINS, help="The tsfel feature domain to be used.")
+    parser.add_argument("-ws", "--window_size", default=100, type=int, help="The window size to be used. (in ms)")
+    parser.add_argument("-wo", "--window_overlap", default=0, type=int, help="The window overlap to be used.")
+    parser.add_argument("-m", "--model", default="RandomForestClassifier", choices=models.MODELS.keys(), help="The model to be used.")
+    parser.add_argument("-o", "--output-filename", type=str, required=True, help="The output filename.")
+    args = parser.parse_args()
+
+    start_end_logger = TimeLogger(f"train_model.py\tStart\t{args}", "train_model.py\tDone\t{duration:0.2f}")
+    start_end_logger.start()
+
+    # Parameters
+    domain = args.domain
+    window_size = args.window_size
+    window_overlap = args.window_overlap
+    model_name = args.model
+    output_filename = get_filename(args.output_filename)
+
+    with TimeLogger("Loading Features", "Done\t{duration:.2f}", separator="\t"):
+        dataset = extract_features.load_features(domain, window_size, window_overlap)
+
+    with TimeLogger("Training Model", "Done\t{duration:.2f}", separator="\t"):
+        model = models.MODELS[model_name]()
+        X = dataset.drop(columns=tsml.ADDITIONAL_COLUMNS, inplace=False)
+        Y = dataset[tsml.LABEL_COLUMN]
+        model.fit(X, Y)
+
+    with TimeLogger("Saving", "Done\t{duration:.2f}", separator="\t"):
+        os.makedirs(os.path.dirname(output_filename), exist_ok=True)
+        with open(output_filename, "wb") as file:
+            pickle.dump(model, file)
+
+    start_end_logger.end()
