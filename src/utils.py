60601d6cd745f8b1bef991ccfd51fe59383dc440
GniLudio <ferdinand.rohlfing@googlemail.com>
2024-09-19 12:31:15 +0200

    Add emg_positioning

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,5 @@
 import time
+import tkinter, tkinter.ttk
 
 class TimeLogger():
     def __init__(self, start_message: str | None, end_message: str | None, separator: str = "\n") -> None:
@@ -27,3 +28,23 @@ class TimeLogger():
             duration = self._end_time - self._start_time
             print(self._end_message.format(start_time=self._start_time, end_time=self._end_time, duration=duration, **kwargs))
 
+def ask_selection(question: str, values: list[str], title: str, font: "tkinter.font._FontDescription", master: tkinter.Tk | None = None) -> str:
+    
+    dialog = tkinter.Toplevel(master=master)
+    dialog.title(title)
+    dialog.update_idletasks()
+    if master:
+        master.eval(f'tk::PlaceWindow {str(dialog)} center')
+
+    tkinter.Label(dialog, text=question, font=font).grid(row=0, column=0, padx=5, pady=5, columnspan=len(values))
+
+    selected: str = None
+    def on_select(selected: str):
+        selected = selected
+        dialog.destroy()
+
+    for i, choice in enumerate(values):
+        tkinter.ttk.Button(dialog, text=str(choice), command=lambda c=choice: on_select(c)).grid(row = 1, column=i, padx=5, pady=5)
+
+    dialog.wait_window()
+    return selected
