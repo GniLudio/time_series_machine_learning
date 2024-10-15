3a04b72fc9f87cb0e611919709fadcce4497098d
studentlab <ferdinand.rohlfing@googlemail.com>
2024-10-15 17:27:52 +0200

    Update feature extraction

--- src/extract_features.py
+++ src/extract_features.py
@@ -1,44 +1,57 @@
 import tsml
 import pandas
-from utils import TimeLogger
+from utils import TimeLogger, join_filename_parts
 from argparse import ArgumentParser
 import tsfel
 import os
-
-def get_features_filename(domain: tsml.TsfelFeatureDomain, window_size: int, window_overlap: int) -> str:
-    return os.path.join(tsml.FEATURES_DIRECTORY, f"d_{domain}-ws_{window_size}-wo_{window_overlap}.csv")
 
 def load_dataset() -> pandas.DataFrame:
     df = pandas.DataFrame()
 
     for filename in os.listdir(tsml.RECORDING_TIMESERIES_DIRECTORY):
-        print(filename)
+        participant = not filename.startswith("Laycee") and filename[4:-6] or filename[18+4:-6]
+        print("\t", participant, filename, flush=True)
+
         df_file = pandas.read_csv(
             filepath_or_buffer=os.path.join(tsml.RECORDING_TIMESERIES_DIRECTORY, filename), 
             index_col=False, 
             dtype=tsml.DATASET_DTYPE,
-            header=59,
+            header=participant != "49" and 59 or 60, # data for participant 49 has an additional header line
             sep="\t",
             usecols = [1, 2, 3, 4, 5, 27],
             names = ["CH1","CH2","CH3","CH4","CH5", "Label"]
         )
-        word_index = 0
+        word_index = 1
         word_start = None
+        df_file["Word Index"] = None
         for i in range(0, len(df_file)):
             if word_start is None:
                 if df_file.loc[i, tsml.LABEL_COLUMN] != 0:
                     word_start = i
             else:
                 if i == len(df_file)-1 or df_file.loc[i+1, tsml.LABEL_COLUMN] != df_file.loc[word_start, tsml.LABEL_COLUMN]:
+                    #if participant == "01" and word_index in [1]:
+                    #    pass
+                    #elif participant == "05" and word_index in [31, 32]:
+                    #    pass
+                    #elif participant == "28" and word_index in [121, 122]:
+                    #    pass
+                    #else:
                     if i-word_start < 3900 or i-word_start > 4100:
-                        print(f"\tStart: {str.ljust(str(word_start+61), 7)}\tEnd: {str.ljust(str(i+61), 7)}\tLength: {str.ljust(str(i-word_start), 7)}")
+                        print(f"\tIndex: {word_index}\tStart: {str.ljust(str(word_start+61), 7)}\tEnd: {str.ljust(str(i+61), 7)}\tLength: {str.ljust(str(i-word_start), 7)}")
+                    else:
+                        pass #print(f"\t{df_file.loc[word_start]['Label']}")
+                    
+                    if participant == "01":
+                        df_file.loc[word_start:i, 'Word Index'] = word_index-1
                     else:
                         df_file.loc[word_start:i, 'Word Index'] = word_index
+
                     word_start = None
-                    word_index = word_index + 1
+                    word_index += 1
         df_file = df_file[df_file['Word Index'].notna()]
-        print(f"\tWord Count: {word_index}")
-        df_file["Filename"] = filename
+        print(f"\tWord Count: {len(df_file['Word Index'].unique())}")
+        df_file["Participant"] = participant
         df = pandas.concat([df, df_file])
     return df
 
@@ -60,16 +73,20 @@ if __name__ == '__main__':
     window_overlap: int = args.window_overlap
     
     # Paths
-    output_filename: str = get_features_filename(
-        domain=domain, 
-        window_size=window_size, 
-        window_overlap=window_overlap, 
-    )
+    output_filename: str = os.path.join(
+        tsml.FEATURES_DIRECTORY, 
+        join_filename_parts({
+            'do': domain,
+            'ws': window_size,
+            'wo': window_overlap
+        })
+    ) + ".csv"
     os.makedirs(os.path.dirname(output_filename), exist_ok=True)
 
     # Load Dataset
     with TimeLogger(start_message="Loading Dataset".ljust(20), end_message="Done\t{duration:.2f}"):
         dataset = load_dataset()
+    print(dataset)
     exit()
 
     # group data
